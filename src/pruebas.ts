26ad1ada4b547925dc7ce4427c34ef5e25608f01
Dario-Lopez-TDF <104174213+Dario-Lopez-TDF@users.noreply.github.com>
2022-05-03 21:35:09 -0300

    03/05/2022 switch

--- src/pruebas.ts
+++ src/pruebas.ts
@@ -1,10 +1,11 @@
-/* Tablas de Multiplicación
+/*Tablas de Multiplicación
 
 • Diseñar un algoritmo que muestre por pantalla la tabla de
 multiplicación del número ingresado por el usuario
 • Para definir hasta qué número desea que muestre la tabla de
 multiplicación, el usuario también deberá ingresar dicho valor
 */
+
 let numTabla: number = Number(prompt("ingresar numero de tabla"));
 let numFinTabla: number = Number(prompt("ingresar hasta que numero"));
 
