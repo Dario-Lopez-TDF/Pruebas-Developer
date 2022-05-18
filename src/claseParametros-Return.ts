93c6390ea1be8c3f8b96e7519e7346c21a995103
Dario-Lopez-TDF <104174213+Dario-Lopez-TDF@users.noreply.github.com>
2022-05-17 21:54:52 -0300

    Clase Parametros y Returns 17/05 repaso con Brian

--- src/claseParametros-Return.ts
+++ src/claseParametros-Return.ts
@@ -4,16 +4,14 @@
 // Programa con funcion que reciba por parametro su nombre y apellido,
 // y lo imprima por consola
 
-function data(name: string, lastname: string, id: string): void {
-  let name: string;
-  let lastname: string;
-  let id: string;
-  id = name + lastname;
+function data(name: string, lastname: string, id: string): string {
+  id = name + " " + lastname;
   return id;
 }
-
+let id: string;
 let nombre: string = prompt("Su Nombre");
 let apellido: string = prompt("Su Apellido");
-let nombreCompleto: string = data(name, lastname, id);
+// la funcion siempre llama a las variable del entorno "declarable"
+let nombreCompleto: string = data(nombre, apellido, id); //id es el retorno
 
 console.log("Su nombre completo es:", nombreCompleto);
