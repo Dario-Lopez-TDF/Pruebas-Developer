421eb01ac96022a6db28f5528628e13bd3fdbea3
Dario-Lopez-TDF <104174213+Dario-Lopez-TDF@users.noreply.github.com>
2022-05-20 20:35:06 -0300

    arreglos et arregloMeses: string[] = new Array(12);

---
+++ src/04-arreglos.ts
@@ -0,0 +1,36 @@
+// clase viernes 20 de Mayo
+// Arrays
+/*
+let arregloMeses: string[] = new Array(12);
+
+arregloMeses[0] = "Enero";
+arregloMeses[1] = "Febrero";
+arregloMeses[2] = "Marzo";
+arregloMeses[3] = "Abril";
+arregloMeses[4] = "Mayo";
+arregloMeses[5] = "Junio";
+arregloMeses[6] = "Julio";
+arregloMeses[7] = "Agosto";
+arregloMeses[8] = "Septiembre";
+arregloMeses[9] = "Octubre";
+arregloMeses[10] = "Noviembre";
+arregloMeses[11] = "Diciembre";
+
+for (let i: number = 0; i < 12; i++) {
+  console.log(arregloMeses[i]);
+}
+*/
+
+let num: string[] = new Array(7);
+
+num[0] = "20 - veinte";
+num[1] = "14 - catorce";
+num[2] = "8 - ocho";
+num[3] = "0 - cero";
+num[4] = "5 - cinco";
+num[5] = "19 - diecinueve";
+num[6] = "24 - veinticuatro";
+
+for (let i: number = 0; i < 6; i++) {
+  console.log(num[i]);
+}
